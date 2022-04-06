33dac664fcfb4fc93c7ca3269c2716b9b286f12f
Christopher Johnson <CLJohnson088@gmail.com>
2022-04-05 19:56:42 -0500

    Added eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended", // https://eslint.org/docs/rules/
+    "plugin:vue/vue3-essential", // https://eslint.vuejs.org/user-guide/#usage
+    "prettier", // https://prettier.io/docs/en/options.html
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
