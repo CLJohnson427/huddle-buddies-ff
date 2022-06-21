1dbb21e8791ce35404b4237d9d495505b58dd866
Christopher Johnson <CLJohnson088@gmail.com>
2022-06-20 20:10:27 -0500

    Added route to Charts page

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,14 @@ const routes = [
   //   name: 'Matchups',
   //   component: () => import(/* webpackChunkName: "about" */ '../views/Matchups.vue')
   // }
+  {
+    path: '/charts',
+    name: 'Charts',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../pages/Charts.vue')
+  }
 ]
 
 const router = createRouter({
