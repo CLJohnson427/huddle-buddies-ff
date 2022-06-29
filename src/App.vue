d77ba65c392008a282fe652279b830ce8f921099
Christopher Johnson <CLJohnson088@gmail.com>
2022-06-28 22:08:32 -0500

    Added SCSS (#15)
    * Added SCSS to the project

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,12 @@
   <router-view/>
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin: 20px 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
 import VueApexCharts from 'vue3-apexcharts';
+import './main.scss';
 
 // Create Vue App
 const app = createApp(App);
