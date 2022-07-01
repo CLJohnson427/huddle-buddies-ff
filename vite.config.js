d5178e60cff4931e70ee86520ca264ff7a5ad192
Christopher Johnson <CLJohnson088@gmail.com>
2022-06-30 21:46:53 -0500

    Added Vue Unplugin Components Library

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,28 @@
-import path from 'path'
+import path from 'path';
 import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 import eslintPlugin from 'vite-plugin-eslint';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/huddle-buddies-ff/' : '/',
-  // plugins: [Vue()],
-  plugins: [Vue(), eslintPlugin()],
   resolve: {
     alias: {
       '@': `${path.resolve(__dirname, 'src')}/`
     },
   },
+  plugins: [
+    // https://github.com/vitejs/vite
+    Vue(),
+
+    // https://github.com/gxmari007/vite-plugin-eslint
+    eslintPlugin(),
+
+    // https://github.com/antfu/unplugin-vue-components
+    Components({
+      extensions: ['vue'],
+      include: [/\.vue$/, /\.vue\?vue/]
+    }),
+  ]
 });
