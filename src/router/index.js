57a5771133a1caf18fd1af0d4c6c9baaae0e1f83
Christopher Johnson <CLJohnson088@gmail.com>
2022-07-02 13:43:35 -0500

    Updated npm packages (#18)
    * Updated NPM packages
    
    * Updated page names and routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  { path: '/', name: 'Home', component: () => import('@/pages/Home.vue') },
-  { path: '/about', name: 'About', component: () => import('@/pages/About.vue') },
-  { path: '/charts', name: 'Charts', component: () => import('@/pages/Charts.vue') },
-  { path: '/:pathMatch(.*)*', name: 'NotFound', component: () => import('@/pages/Home.vue') }
+  { path: '/', name: 'Home', component: () => import('@/pages/HomePage.vue') },
+  { path: '/about', name: 'About', component: () => import('@/pages/AboutPage.vue') },
+  { path: '/charts', name: 'Charts', component: () => import('@/pages/ChartsPage.vue') },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: () => import('@/pages/HomePage.vue') }
 ]
 
 const router = createRouter({
