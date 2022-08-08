a5d9117806b914bde9625238ba35eb56d2e66eb8
Christopher Johnson <CLJohnson088@gmail.com>
2022-08-08 15:46:29 -0500

    Updated eslint rules (#25)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,14 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended", // https://eslint.org/docs/rules/
-    "plugin:vue/vue3-essential", // https://eslint.vuejs.org/user-guide/#usage
-    "prettier", // https://prettier.io/docs/en/options.html
+    'eslint:recommended', // https://eslint.org/docs/rules/
+    'plugin:vue/vue3-essential' // https://eslint.vuejs.org/user-guide/#usage
   ],
   rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "brace-style": ["error", "stroustrup", { allowSingleLine: true }]
+    // 'ruleName': ['off, warn, error', { ruleOptions }]
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
+    'no-unused-vars': ['warn']
   },
 };
