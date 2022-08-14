7faefa357055bfb4ac8d4d79749fb967d33d6286
Christopher Johnson <CLJohnson088@gmail.com>
2022-08-13 22:27:37 -0500

    Standings Interfaces

---
+++ src/data/Types/StandingsInterfaces.ts
@@ -0,0 +1,48 @@
+import { TeamRoster } from "@/data/types/RosterInterface";
+import { LeagueManager } from '@/data/types/UserInterfaces'
+
+/**********************************************************
+  Custom Interfaces
+/*********************************************************/
+export interface WeeklyStanding {
+  medianLosses: number;
+  medianTies: number;
+  medianWins: number;
+  playerLosses: number;
+  playerTies: number;
+  playerWins: number;
+  pointsAgainst: number;
+  pointsFor: number;
+  totalLosses: number;
+  totalTies: number;
+  totalWins: number;
+  week: number;
+}
+
+export interface TeamStanding {
+  divisionWins: number;
+  divisionLosses: number;
+  divisionTies: number;
+  manager: LeagueManager;
+  medianWins: number;
+  medianLosses: number;
+  medianTies: number;
+  playerWins: number;
+  playerLosses: number;
+  playerTies: number;
+  rosterId: number;
+  totalWins: number;
+  totalLosses: number;
+  totalTies: number;
+  totalPointsFor: number;
+  totalPointsAgainst: number;
+  weeklyStandings: WeeklyStanding[];
+}
+
+export interface Standings {
+  league_id: string;
+  medianMatch: boolean;
+  roster: TeamRoster;
+  seasonYear: string | number;
+  standings: Map<string, TeamStanding>;
+}
