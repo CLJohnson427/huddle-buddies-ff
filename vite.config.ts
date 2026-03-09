cf3cd6d3340326be2dc9c39e28e14cc62606bf9b
Christopher Johnson <github@cljohnson.net>
2026-03-09 15:35:00 -0500

    Update the base value in vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: process.env.NODE_ENV === 'production' ? '/huddle-buddies-ff/' : '/',
-  base: process.env.NODE_ENV === 'production' ? './' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/huddle-buddies-ff/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? './' : '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
